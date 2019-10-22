e3364b9648016728c61b7a5ac623fa4735c2808c
Evgeniy Danilchenko <jodan@aaanet.ru>
2019-10-22 08:23:53 +0300

    mobile version of the home page

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import "./index.scss";
 import "./pages/landing/landing.js";
 import "./layout/home.js";
+import "./modules/header.js";
---
+++ src/modules/header.js
@@ -0,0 +1,55 @@
+"use strict";
+
+const anchors = document.querySelectorAll('a[href*="#"]');
+
+for (let anchor of anchors) {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault();
+
+    const blockID = anchor.getAttribute("href").substr(1);
+
+    document.getElementById(blockID).scrollIntoView({
+      behavior: "smooth",
+      block: "start"
+    });
+  });
+}
+window.addEventListener("DOMContentLoaded", () => {
+  const menu = document.querySelector(".nav"),
+    menuItem = document.querySelectorAll(".nav_item"),
+    button = document.querySelector(".header-login__guest"),
+    hamburger = document.querySelector(".hamburger");
+
+  hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("hamburger_active");
+    button.classList.toggle("header-login__guest_active");
+    menu.classList.toggle("nav_active");
+  });
+
+  function addListenerMulti(element, eventNames, listener) {
+    let events = eventNames.split(" ");
+    for (let i = 0, iLen = events.length; i < iLen; i++) {
+      element.addEventListener(events[i], listener, false);
+    }
+  }
+
+  addListenerMulti(window, "scroll touchstart", function() {
+    if (hamburger.classList.contains("hamburger_active")) {
+      hamburger.classList.remove("hamburger_active");
+    }
+    if (menu.classList.contains("nav_active")) {
+      menu.classList.remove("nav_active");
+    }
+    if (button.classList.contains("header-login__guest_active")) {
+      button.classList.remove("header-login__guest_active");
+    }
+  });
+
+  menuItem.forEach(item => {
+    item.addEventListener("click", () => {
+      hamburger.classList.toggle("hamburger_active");
+      button.classList.toggle("header-login__guest_active");
+      menu.classList.toggle("nav_active");
+    });
+  });
+});
